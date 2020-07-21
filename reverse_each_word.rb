f4eee8501a9c1ce571006e0544f988d4d86c971e
Justin Webb <webb.n.justin@gmail.com>
2020-07-21 20:30:06 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,7 @@ require "pry"
 def reverse_each_word (string)
   string_split = string.split(" ")
   
-  array = string_split.collect do |word|
+  string_split.collect do |word|
     word.reverse
   end.join(" ")
    
