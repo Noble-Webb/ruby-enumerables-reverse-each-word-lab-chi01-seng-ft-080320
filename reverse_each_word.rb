d58b32abdedcf2f017a9af6878ff529217d9881e
Justin Webb <webb.n.justin@gmail.com>
2020-07-21 20:17:42 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,12 +2,12 @@ require "pry"
 def reverse_each_word (string)
   string_split = string.split(" ")
   binding.pry
-  i = 0
-  reversed = ""
-  string_split.collect do
-    char = string_split[0][i]
-    reversed  << char 
+  #i = 0
+  #reversed = ""
+  #string_split.collect do
+  #  char = string_split[0][i]
+  #  reversed  << char 
     
-   i += 1
+  # i += 1
  end
 end
