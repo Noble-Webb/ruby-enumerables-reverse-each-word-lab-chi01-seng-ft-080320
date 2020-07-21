b2e80eab1f4114c4a0ee8b959a3f6b0b4cfefaf6
Justin Webb <webb.n.justin@gmail.com>
2020-07-21 20:16:59 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,25 +1,13 @@
 require pry 
 def reverse_each_word (string)
   string_split = string.split(" ")
+  binding.pry
   i = 0
   reversed = ""
   string_split.collect do
-    binding.pry
     char = string_split[0][i]
     reversed  << char 
     
    i += 1
  end
 end
-
-def reverse_each_word (string)
-  reversed = ""
-  string_split = string.split(" ")
-  
-  string_split.collect { 
-    reversed << string_split.to_s
-  }
-    binding.pry
-    reversed.join
-    
-    
